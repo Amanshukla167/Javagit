c3689b907ae22041d31ddf45112eed9a015ae46b
Aman Shukla <amanshukla167@gmail.com>
2024-11-23 01:27:45 +0530

    Hi this is my frist coomit in java

--- src/main/java/MavenDemo/com/App.java
+++ src/main/java/MavenDemo/com/App.java
@@ -26,6 +26,7 @@ public class App
     public static void main( String[] args )throws Exception
     {
         System.out.println( "Hello World!" );
+        System.out.println("you have publis to git repo");
         
         int emp_id = 104;
         String emp_name = "Anushree";
---
+++ src/main/java/MavenDemo/com/gitTest.java
@@ -0,0 +1,11 @@
+package MavenDemo.com;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is your frist git");
+
+	}
+
+}
